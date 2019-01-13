2ca81e8e1b38d06588ea5f79ee555d649710c7d8
ty956132057 <956132057@qq.com>
2019-01-13 13:29:11 +0800

    登陆页面结构完成

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,95 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-  
+  <div class="login-container">
+    <div class="form-container">
+      <h2>账号登陆</h2>
+      <el-form
+        label-position="top"
+        :rules="rules"
+        ref="formData"
+        label-width="80px"
+        :model="formData"
+      >
+        <el-form-item label="账号" prop="userName">
+          <el-input v-model="formData.userName"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop="passWord">
+          <el-input v-model="formData.passWord"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" class="login-btn" @click="submitForm('formData')">登陆</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
   </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'app',
- 
-}
+  name: "app",
+  data() {
+    return {
+      formData: {
+        userName: "",
+        passWord: ""
+      },
+      rules: {
+        userName: [
+          { required: true, message: "账号长度过低", trigger: "change" },
+          {
+            min: 3,
+            max: 10,
+            message: "长度在 3 到 10个字符",
+            trigger: "change"
+          }
+        ],
+        passWord: [
+          {
+            required: true,
+            message: "密码长度不够,安全系数过低",
+            trigger: "change"
+          },
+          {
+            min: 6,
+            max: 10,
+            message: "长度在 3 到 10个字符",
+            trigger: "change"
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formData) {
+      this.$refs[formData].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    }
+  }
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+.login-container {
+  width: 100%;
+  height: 100%;
+  background-color: skyblue;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .form-container {
+    width: 580px;
+    padding: 40px;
+    box-sizing: border-box;
+    background-color: #fff;
+    border-radius: 10px;
+    .login-btn {
+      width: 100%;
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
+// 导入elmet
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+// 导入css
+import "./assets/base.css"
 
 new Vue({
   render: h => h(App),
